e7fe10e0bb43d2d47a2b0fb83d6a2825709f4bdc
rceco <rafaelcecotti@gmail.com>
2019-12-06 16:07:30 +0100

    first commit

---
+++ src/main/java/miage/rcec/bowling/App.java
@@ -0,0 +1,13 @@
+package miage.rcec.bowling;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
